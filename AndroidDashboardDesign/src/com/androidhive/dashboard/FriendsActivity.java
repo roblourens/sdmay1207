9ad7215a0cde25eb2b8ca79f280a532f641fa608
asjagdeo <asjagdeo@f442a788-03c2-4c86-abaf-9b69642281cc>
2012-03-19 08:52:37 +0000

    Added a node list that has buttons
    git-svn-id: https://source.ece.iastate.edu/svn/sdmay1207@65 f442a788-03c2-4c86-abaf-9b69642281cc

--- AndroidDashboardDesign/src/com/androidhive/dashboard/FriendsActivity.java
+++ AndroidDashboardDesign/src/com/androidhive/dashboard/FriendsActivity.java
@@ -2,13 +2,33 @@ package com.androidhive.dashboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ListView;
 import androidhive.dashboard.R;
 
 public class FriendsActivity extends Activity {
 	 /** Called when the activity is first created. */
+	ListView lstView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.friends_layout);
+    	  super.onCreate(savedInstanceState);
+	        setContentView(R.layout.friends_layout);
+	        Button btn = (Button) findViewById(R.id.btn);
+
+
+	        btn.setOnTouchListener(new OnTouchListener() {
+
+
+				public boolean onTouch(View arg0, MotionEvent arg1) {
+					// TODO Auto-generated method stub
+					// TODO Auto-generated method stub
+					setContentView(R.layout.news_feed_layout);
+					return false;
+				}
+
+	        });
     }
 }
