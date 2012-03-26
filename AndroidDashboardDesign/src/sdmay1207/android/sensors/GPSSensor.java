33f67975308e75d81059875a777d6231569adc5e
rlourens <rlourens@f442a788-03c2-4c86-abaf-9b69642281cc>
2012-03-26 00:58:27 +0000

    put this back
    git-svn-id: https://source.ece.iastate.edu/svn/sdmay1207@98 f442a788-03c2-4c86-abaf-9b69642281cc

---
+++ AndroidDashboardDesign/src/sdmay1207/android/sensors/GPSSensor.java
@@ -0,0 +1,71 @@
+package sdmay1207.android.sensors;
+
+import sdmay1207.ais.sensors.GPS;
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class GPSSensor extends GPS implements LocationListener
+{
+
+    private LocationManager locManager;
+
+    /*
+     * Subscribes to the LocationManager updater to receive GPS outputs
+     */
+    public GPSSensor(Context c)
+    {
+        locManager = (LocationManager) c
+                .getSystemService(Context.LOCATION_SERVICE);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0,
+                this);
+    }
+
+    /*
+     * Reads latest output and returns a new Location object
+     */
+    @Override
+    public Location getReading()
+    {
+        android.location.Location location = locManager
+                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null)
+        {
+            return new Location(location.getLatitude(),
+                    location.getLongitude());
+        } else
+        {
+            return new Location(0.0, 0.0);
+        }
+    }
+
+    /*
+     * Returns units in degrees
+     */
+    @Override
+    public String getUnits()
+    {
+        return "degrees";
+    }
+
+    //@Override
+    public void onLocationChanged(android.location.Location location)
+    {
+    }
+
+    //@Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    //@Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+   // @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+}
