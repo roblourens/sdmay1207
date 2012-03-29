3618278a010ffd26798fb493cb1f73c10483b986
rlourens <rlourens@f442a788-03c2-4c86-abaf-9b69642281cc>
2012-03-29 16:41:13 +0000

    a couple changes to Notifications before adding Point2PointGui
    git-svn-id: https://source.ece.iastate.edu/svn/sdmay1207@125 f442a788-03c2-4c86-abaf-9b69642281cc

--- AndroidDashboardDesign/src/com/androidhive/dashboard/DashboardApplication.java
+++ AndroidDashboardDesign/src/com/androidhive/dashboard/DashboardApplication.java
@@ -15,9 +15,11 @@ import sdmay1207.ais.NodeController;
 import sdmay1207.ais.network.NetworkController.Event;
 import sdmay1207.ais.network.NetworkController.NetworkEvent;
 import sdmay1207.ais.network.model.NetworkMessage;
+import sdmay1207.ais.sensors.GPS.Location;
 import sdmay1207.android.sensors.BatterySensor;
 import sdmay1207.android.sensors.CompassSensor;
 import sdmay1207.android.sensors.GPSSensor;
+import sdmay1207.networkrejoining.NetworkRejoinMonitor.NetworkRejoinListener;
 import android.app.Application;
 import android.util.Log;
 
@@ -71,7 +73,8 @@ public class DashboardApplication extends Application
         System.out.println("Node #: " + nodeNumber);
     }
 
-    public class NotificationManager extends Observable implements Observer
+    public class NotificationManager extends Observable implements Observer,
+            NetworkRejoinListener
     {
         public List<Notification> notifications = new ArrayList<Notification>();
 
@@ -84,6 +87,7 @@ public class DashboardApplication extends Application
             nc.addNetworkObserver(this);
         }
 
+        // Listen for events from the networkController
         public void update(Observable observable, Object obj)
         {
             NetworkEvent netEvent = (NetworkEvent) obj;
@@ -94,21 +98,62 @@ public class DashboardApplication extends Application
             // Once we implement NetworkRejoinListener, maybe we can ditch these
             // joined/left events
             if (Arrays.asList(eventsToBeNotifiedAbout).contains(netEvent.event))
-            {
-                Notification n = new Notification(netEvent);
-                notifications.add(0, n);
+                addNotification(new NetworkEventNotification(netEvent));
+        }
 
-                setChanged();
-                notifyObservers(n);
-            }
+        public void addNotification(Notification n)
+        {
+            notifications.add(0, n);
+
+            setChanged();
+            notifyObservers(n);
+        }
+
+        public void lostSingleNode()
+        {
+            // ignore
+        }
+
+        public void networkSplit(final Location p)
+        {
+            addNotification(new Notification()
+            {
+                @Override
+                public String shortDisplayString()
+                {
+                    return "We lost several nodes at once - you should move to "
+                            + p + " to rejoin them.";
+                }
+            });
+        }
+
+        public void lostEntireNetwork(final Location p)
+        {
+            addNotification(new Notification()
+            {
+                @Override
+                public String shortDisplayString()
+                {
+                    return "The entire network has become disconnected - you should move to "
+                            + p + " to rejoin it.";
+                }
+            });
         }
     }
 
-    public class Notification
+    // complicated - we could just use strings here but we might want to add
+    // some extra behavior for a notification later - String of extra details,
+    // timestamps, whatever, so I figure set up a simple object for now
+    public abstract class Notification
+    {
+        public abstract String shortDisplayString();
+    }
+
+    public class NetworkEventNotification extends Notification
     {
         public NetworkEvent netEvent;
 
-        public Notification(NetworkEvent netEvent)
+        public NetworkEventNotification(NetworkEvent netEvent)
         {
             this.netEvent = netEvent;
         }
