7a33aea08ce82d2ed4fc2dcb0d96e9863d7e2780
rlourens <rlourens@f442a788-03c2-4c86-abaf-9b69642281cc>
2012-04-06 19:16:18 +0000

    fix GPS test app to stop asking for GPS updates on close
    git-svn-id: https://source.ece.iastate.edu/svn/sdmay1207@158 f442a788-03c2-4c86-abaf-9b69642281cc

--- GPS/src/android/GPS/view/GPSActivity.java
+++ GPS/src/android/GPS/view/GPSActivity.java
@@ -11,60 +11,77 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class GPSActivity extends Activity implements LocationListener {
-	Button button;
-	LocationManager locManager;
-	
+public class GPSActivity extends Activity implements LocationListener
+{
+    Button button;
+    LocationManager locManager;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         ButtonListener listener = new ButtonListener(this);
-		button = (Button) findViewById(R.id.retrieveButton);
-		button.setOnClickListener(listener);
-		
-		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, this);
+        button = (Button) findViewById(R.id.retrieveButton);
+        button.setOnClickListener(listener);
+
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
     }
 
-	public void clickConnect() {
-		Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		if(location!=null){
-			this.showLocation(location);
-		}
-	} 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                this);
+    }
 
-	@Override  
-	public void onLocationChanged(Location location) {
-	    System.out.println("Location changed");
-	}
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        locManager.removeUpdates(this);
+    }
 
-	@Override
-	public void onProviderDisabled(String arg0) {}
+    public void clickConnect()
+    {
+        Location location = locManager
+                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null)
+        {
+            this.showLocation(location);
+        }
+    }
 
-	@Override
-	public void onProviderEnabled(String arg0) {}
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        System.out.println("Location changed");
+    }
 
-	@Override
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
-	
-	private void showLocation(Location location){
-		EditText gpsText = (EditText) findViewById(R.id.gpsCoords); 
-		Double latitude = location.getLatitude();
-		Double longitude = location.getLongitude();
-		gpsText.append("> Lat: "+latitude+"\n   Long: "+longitude+"\n");
-	}
+    @Override
+    public void onProviderDisabled(String arg0)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2)
+    {
+    }
+
+    private void showLocation(Location location)
+    {
+        EditText gpsText = (EditText) findViewById(R.id.gpsCoords);
+        Double latitude = location.getLatitude();
+        Double longitude = location.getLongitude();
+        gpsText.append("> Lat: " + latitude + "\n   Long: " + longitude + "\n");
+    }
 }
-
-
-
-
-
-
-
-
-
-
